fc12393fda520da2a8ffee735ac4528fe9f261d8
Miranto <hei.finoana.5@gmail.com>
2026-05-11 21:17:07 +0300

    fix: session,speaker service and session route

--- event-page-manager/app/api/sessions/route.ts
+++ event-page-manager/app/api/sessions/route.ts
@@ -1,5 +1,4 @@
-
-=import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { sessionService } from "@/app/lib/service/session.service";
 import { handleError } from "@/app/lib/service.http";
 import type {
